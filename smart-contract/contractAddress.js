839fbf89a61e7d148f104750e6586c2d98f1011b
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-24 22:04:48 +0300

    😊 add smart-contracts

---
+++ smart-contract/contractAddress.js
@@ -0,0 +1,5 @@
+
+    export const taalmQuestsAddress = "0xD0a999DB766c1a41Db113f725a851eb3ea6A2b3a";
+    export const taalmMembershipAddress = "0x1fC0d18d6045DCBD44933889496d5cf5F5360010";
+    export const taalmFactoryAddress = "0x7771117a5171B643C0aDaEfa863703ac929c2Ccf";
+    
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,28 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
-
+const fs = require("fs");
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const TaalmQuests = await hre.ethers.getContractFactory("TaalmQuests");
+  const taalmQuests = await TaalmQuests.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await taalmQuests.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("taalmQuests deployed to:", taalmQuests.address);
 
-  await lock.deployed();
+  const TaalmMembership = await hre.ethers.getContractFactory("TaalmMembership");
+  const taalmMembership = await TaalmMembership.deploy();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await taalmMembership.deployed();
+
+  console.log("taalmMembership deployed to:", taalmMembership.address);
+
+  const TaalmFactory = await hre.ethers.getContractFactory("TaalmFactory");
+  const taalmFactory = await TaalmFactory.deploy();
+
+  await taalmFactory.deployed();
+
+  console.log("taalmFactory deployed to:", taalmFactory.address);
+
+
+
+  fs.writeFileSync(
+    "./contractAddress.js", `
+    export const taalmQuestsAddress = "${taalmQuests.address}";
+    export const taalmMembershipAddress = "${taalmMembership.address}";
+    export const taalmFactoryAddress = "${taalmFactory.address}";
+    `
+  )
+
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
